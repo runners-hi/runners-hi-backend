37cdc9a11ff0a54c9d8a48060ec083534086e851
정진영 <rachel040131@gmail.com>
2026-01-29 12:21:16 +0900

    docs(region): 동 단위 확장 TODO 주석 추가

--- src/main/kotlin/com/runnershi/domain/region/entity/Region.kt
+++ src/main/kotlin/com/runnershi/domain/region/entity/Region.kt
@@ -9,6 +9,8 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 
+// TODO: 동 단위 확장 시 districts 테이블 추가 (region_id FK, 1:N 관계)
+//       - 시 선택 후 해당 시의 동만 조회: GET /api/regions/{regionId}/districts
 @Entity
 @Table(name = "regions")
 class Region(
