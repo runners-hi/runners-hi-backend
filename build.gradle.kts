9efdfe147fe9f3766329e5c47d3193268f9ebb63
정진영 <rachel040131@gmail.com>
2026-02-11 09:59:13 +0900

    test(auth): Auth 도메인 테스트 코드 작성 #98

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testImplementation("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- src/test/kotlin/com/runnershi/support/ControllerTest.kt
+++ src/test/kotlin/com/runnershi/support/ControllerTest.kt
@@ -1,6 +1,6 @@
 package com.runnershi.support
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import tools.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
